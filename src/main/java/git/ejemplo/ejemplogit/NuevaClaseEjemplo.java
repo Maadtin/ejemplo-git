d2cb732f8857f595391158263162879d67cb2323
Martin <martinetex18@gmail.com>
2017-11-12 20:23:37 +0100

    Creada clase de ejemplo

---
+++ src/main/java/git/ejemplo/ejemplogit/NuevaClaseEjemplo.java
@@ -0,0 +1,12 @@
+package git.ejemplo.ejemplogit;
+
+public class NuevaClaseEjemplo {
+
+
+	String nombre = "Martin";
+
+	public NuevaClaseEjemplo () {
+		System.out.println("Hola yo me llamo" + this.nombre);
+	}
+
+}
