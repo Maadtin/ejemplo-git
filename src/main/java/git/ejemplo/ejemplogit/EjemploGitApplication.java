ce4f5182929c88112f950bd38441eeba5042279a
Martin <martinetex18@gmail.com>
2017-11-12 18:58:37 +0100

    Primer commit

---
+++ src/main/java/git/ejemplo/ejemplogit/EjemploGitApplication.java
@@ -0,0 +1,14 @@
+package git.ejemplo.ejemplogit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploGitApplication.class, args);
+	}
+
+
+}
