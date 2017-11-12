3698ce170b05275dba9bd9047e01809fae31e32d
albertserrano <serrano.ag6@gmail.com>
2017-11-12 20:36:13 +0100

    Añadimos un nombre nuevo

--- src/main/java/git/ejemplo/ejemplogit/NuevaClaseEjemplo.java
+++ src/main/java/git/ejemplo/ejemplogit/NuevaClaseEjemplo.java
@@ -3,7 +3,13 @@ package git.ejemplo.ejemplogit;
 public class NuevaClaseEjemplo {
 
 
-	String nombre = "Martin";
+	String nombre1 = "Martin";
+
+	public NuevaClaseEjemplo () {
+		System.out.println("Hola yo me llamo" + this.nombre);
+	}
+
+	String nombre2 = "Albert";
 
 	public NuevaClaseEjemplo () {
 		System.out.println("Hola yo me llamo" + this.nombre);
